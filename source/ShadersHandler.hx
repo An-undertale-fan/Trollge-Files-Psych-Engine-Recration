c5abe76f294d264b754200b2942a036ce3468b24
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2023-01-01 09:45:11 +0200

    Update ShadersHandler.hx

--- source/ShadersHandler.hx
+++ source/ShadersHandler.hx
@@ -2,13 +2,11 @@ package;
 
 import openfl.display.Shader;
 import openfl.filters.ShaderFilter;
-import Vcr;
 import ChromaticAberration;
 
 class ShadersHandler
 {
 	public static var chromaticAberration:ShaderFilter = new ShaderFilter(new ChromaticAberration());
-	public static var vcr:ShaderFilter = new ShaderFilter(new Vcr());
 
 	public static function setChrome(chromeOffset:Float):Void
 	{
@@ -16,10 +14,4 @@ class ShadersHandler
 		chromaticAberration.shader.data.gOffset.value = [0.0];
 		chromaticAberration.shader.data.bOffset.value = [chromeOffset * -1];
 	}
-    public static function setNoise(value:Float) {
-        vcr.shader.data.noisePercent.value[0] = value;
-    }
-    public static function setITime(value:Float) {
-        vcr.shader.data.iTime.value[0] = value;
-    }
 }
